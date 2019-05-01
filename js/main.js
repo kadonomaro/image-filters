8defc1adc729397491ae9d5a767bcd2443224e53
kadonomaro <denis.afer@gmail.com>
2019-05-01 14:25:10 +0700

    Initial

---
+++ js/main.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function () {
+   
+    const canvas = document.querySelector('.canvas');
+    const context = canvas.getContext('2d');
+    const x = 0;
+    const y = 0;
+
+});
