4eb01dd2191bbbb5429dd6bec91fafc842b803b6
kadonomaro <denis.afer@gmail.com>
2019-05-01 17:14:34 +0700

    create average-color func

--- js/main.js
+++ js/main.js
@@ -14,8 +14,11 @@ document.addEventListener('DOMContentLoaded', function () {
     let imageData = context.getImageData(x, y, image.width, image.height);
 
     // imageFilters.grayscale(imageData.data);
-    imageFilters.opacity(imageData.data, 100);
-    imageFilters.invert(imageData.data);
+    // imageFilters.opacity(imageData.data, 100);
+    // imageFilters.invert(imageData.data);
+    // imageFilters.some(imageData.data);
+    // imageFilters.red(imageData.data);
+    imageFilters.averageColor(imageData.data);
 
     context.putImageData(imageData, x, y);
     console.log(imageData.data);
@@ -25,38 +28,86 @@ document.addEventListener('DOMContentLoaded', function () {
 let imageFilters = {
 
     grayscale: function (data) {
+        let avg;
         for (let i = 0; i < data.length; i += 4) {
-            const grayscale =
-                0.34 * data[i] + 0.5 * data[i + 1] * 0.16 + data[i + 2];
-            data[i] = grayscale;
-            data[i + 1] = grayscale;
-            data[i + 2] = grayscale;
-            
+            avg = (data[i] + data[i + 1] + data[i + 2]) / 3 ;
+            data[i] = avg;
+            data[i + 1] = avg;
+            data[i + 2] = avg;
         }
     },
+
     opacity: function (data, opacity = 100) {
         opacity *= 2.55;
         for (let i = 0; i < data.length; i += 4) {
             data[i + 3] = opacity;
         }
     },
-    some: function (data) {
-        let temp;
-        
-        for (let i = 0; i < data.length; i += 4) {
-            temp = data[i];
-            data[i] = data[i + 1];
-            data[i + 1] = data[i + 2];
-            data[i + 2] = temp;
 
-            
-        }
-    },
     invert: function (data) {
         for (let i = 0; i < data.length; i += 4) {
             data[i] = data[i] ^ 255;
             data[i + 1] = data[i + 1] ^ 255;
             data[i + 2] = data[i + 2] ^ 255;
+        }
+    },
+
+    some: function (data) {
+        let temp;
+        let avg;
+        
+        // for (let i = 0; i < data.length; i += 12) {
+        //     avg = ((data[i] + data[i + 1] + data[i + 2]) + 
+        //         (data[i + 4] + data[i + 5] + data[i + 6]) + 
+        //         (data[i + 8] + data[i + 9] + data[i + 10])) / 9;
+        //     data[i] = avg;
+        //     data[i + 1] = avg;
+        //     data[i + 2] = avg;
+        //     data[i + 4] = avg;
+        //     data[i + 5] = avg;
+        //     data[i + 6] = avg;
+        //     data[i + 8] = avg;
+        //     data[i + 9] = avg;
+        //     data[i + 10] = avg;
+        // }
+        for (let i = 0; i < data.length; i += 4) {
+            avg = ((data[i] + data[i + 1] + data[i + 2]) + 
+                (data[i + 4] + data[i + 5] + data[i + 6]) + 
+                (data[i + 8] + data[i + 9] + data[i + 10])) / 9;
+            data[i] = avg;
+            data[i + 1] = avg;
+            data[i + 2] = avg;
+            data[i + 4] = avg;
+            data[i + 5] = avg;
+            data[i + 6] = avg;
+            data[i + 8] = avg;
+            data[i + 9] = avg;
+            data[i + 10] = avg;
+        }
+    },
+    red: function (data) {
+        for (let i = 0; i < data.length; i +=4 ) {
+            data[i + 1] = 0;
+            data[i + 2] = 0;
+            
+        }
+    },
+    averageColor: function (data) {
+        let temp = [0, 0, 0];
+        for (let i = 0; i < data.length; i += 4) {
+            temp[0] += data[i];
+            temp[1] += data[i + 1];
+            temp[2] += data[i + 2];
+        }
+        temp[0] /= (data.length / 4);
+        temp[1] /= (data.length / 4);
+        temp[2] /= (data.length / 4);
+        console.log(temp);
+        for (let i = 0; i < data.length; i += 4) {
+            data[i] = temp[0];
+            data[i + 1] = temp[1];
+            data[i + 2] = temp[2];
+            
         }
     }
 };
