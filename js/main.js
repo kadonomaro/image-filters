cd361ce49cb7edc2e9cf830a6c8a19220e3eef49
kadonomaro <denis.afer@gmail.com>
2019-05-01 15:10:07 +0700

    create grayscale filter

--- js/main.js
+++ js/main.js
@@ -4,5 +4,35 @@ document.addEventListener('DOMContentLoaded', function () {
     const context = canvas.getContext('2d');
     const x = 0;
     const y = 0;
+    let image = document.querySelector('.image');
+    
+    canvas.width = image.width;
+    canvas.height = image.height;
+
+    context.drawImage(image, x, y);
+
+    let imageData = context.getImageData(x, y, image.width, image.height);
+
+    imageFilters.grayscale(imageData.data);
+
+
+    context.putImageData(imageData, x, y);
 
 });
+
+let imageFilters = {
+    i: 0,
+    red: this.i,
+    green: this.i + 1,
+    blue: this.i + 2,
+    alpha: this.i + 3,
+    grayscale: function (data) {
+        for (let i = 0; i < data.length; i += 4) {
+            const brightness =
+                0.34 * data[i] + 0.5 * data[i + 1] * 0.16 + data[i + 2];
+            data[i] = brightness;
+            data[i + 1] = brightness;
+            data[i + 2] = brightness;
+        }
+    }
+};
